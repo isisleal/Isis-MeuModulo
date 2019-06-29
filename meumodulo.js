8d19045ed2d04b6bb7a0a9e3f5d3baa0d3eaed57
isisleal <isisleal@gmail.com>
2019-06-28 22:32:55 -0300

    codigook

--- meumodulo.js
+++ meumodulo.js
@@ -1,30 +1,39 @@
-const fetch = require('node-fetch');
-var cheerio = require('cheerio');
+const request = require('request');
+const cheerio = require('cheerio');
 
-// fetch(`https://www.submarino.com.br`, {method: 'GET'})
-// .then(res => 
-//      res.text() )
-// .then(body => {
-//     return body})
-// .catch(e => {
-//     return console.log(e)})
-    
-    // $ = cheerio.load('html');
-    // $('h2[class=shelf-title]').html()
+const urlProduct = 'https://www.americanas.com.br/produto/133718358/'
 
-    const https = require("https")
+request (urlProduct, (error, response, html) => { 
+    if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
 
-    const url = "https://www.americanas.com.br/produto/133718358/";
-
-    https.get(url, res => {
-        res.setEncoding("utf8");
+        const mArray = []
         
-        let body = "";
-        res.on("data", data => {
-            body += data;
-        });
-        res.on("end", () => {
-            console.log(body);
-        });
-    })
+        $('.fbPVXQ').each(function(i) {
+            mArray.push($( this ).text()) 
+             });
+             
+
+        const jsonProduct = {
+
+        id: parseInt( $('.brNcBx').text().substring(5, 14) ) ,
+
+        breadcrumb: mArray,
+
+        name: $('.product-title__TitleUI-sc-116vf1e-0').text(),
+
+        img: $('img').attr('src'),
+
+        seller: $('.seller-00776574000660').text().repeat(1),
+
+        price:  parseFloat ($('.main-offer__SalesPrice-sc-1oo1w8r-1').text().substring(2)),
+
+        }
+
+        console.log(jsonProduct)
+        
+    }
+} ) 
+
+
 
